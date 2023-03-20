0409a033af813cd75e60cac14ba7b155ff757236
Tirsa-31 <tirsasanchez1g@gmail.com>
2023-03-20 12:19:05 -0600

    mate actividad

---
+++ mate31/mate31_resources/js/ardoraColoreaCFG.js
@@ -0,0 +1,36 @@
+//Creado con Ardora - www.webardora.net
+//bajo licencia Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)
+//para otros usos contacte con el autor
+var timeAct=360; timeIni=360; timeBon=0;
+var successes=0; successesMax=0; attempts=0; attemptsMax=1;
+var score=0; scoreMax=1; scoreInc=1; scoreDec=1
+var typeGame=0;
+var tiTime=false;
+var tiTimeType=0;
+var tiButtonTime=true;
+var textButtonTime="Comenzar";
+var tiSuccesses=true;
+var tiAttempts=false;
+var tiScore=true;
+var startTime;
+var colorBack="#75D977"; colorButton="#2E7B98"; colorText="#000000"; colorSele="#1C57E3";
+var goURLNext=false; goURLRepeat=false;tiAval=false;
+var scoOk=0; scoWrong=0; scoOkDo=0; scoWrongDo=0; scoMessage=""; scoPtos=10;
+var fMenssage="Verdana, Geneva, sans-serif";
+var fActi="Verdana, Geneva, sans-serif";
+var fEnun="Verdana, Geneva, sans-serif";
+var timeOnMessage=5; messageOk="Bien hecho"; messageTime="Tardaste mucho"; messageError="Error"; messageErrorG="Error"; messageAttempts="Lo intentaste"; isShowMessage=false;
+var urlOk=""; urlTime=""; urlError=""; urlAttempts="";
+var goURLOk="_blank"; goURLTime="_blank"; goURLAttempts="_blank"; goURLError="_blank"; 
+borderOk="#008000"; borderTime="#FF0000";borderError="#FF0000"; borderAttempts="#FF0000";
+var wordsGame="bWF0ZTMx"; wordsStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+function giveZindex(typeElement){var valueZindex=0; capas=document.getElementsByTagName(typeElement);
+for (i=0;i<capas.length;i++){if (parseInt($(capas[i]).css("z-index"),10)>valueZindex){valueZindex=parseInt($(capas[i]).css("z-index"),10);}}return valueZindex;}
+var joinPar=[];
+var c=["#F2CB2D","#933ED9","#40E86A","#F064F0","#495FF3","#D70B76","#ED3F3F"];
+var x=["31","8","55","69","98","151","148"];
+var y=["85","25","33","89","106","107","186"];
+var t=["4","25","12","5","20","16","18"];
+var cM=["#F2CB2D","#933ED9","#40E86A","#F064F0","#495FF3","#D70B76","#ED3F3F"];
+var tM=["2+2","5X5","4x3","7-2","10X2","2X8","9X2"];
+var indexColor=0;
